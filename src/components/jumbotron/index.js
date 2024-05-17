f1bea35181cb7c0e3994690070a5d28e03cf9545
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-05-17 13:50:54 +0530

    Add - normalize.css

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -4,7 +4,7 @@ import { Container, Inner, Item, Pane, Title, SubTitle, Image } from "./styles/j
 export default function Jumbotron ( { children, direction = 'row', ...restProps } ){
     return (
 
-        <Item direction = { direction } >
+        <Item direction = { direction } {...restProps}>
             <Inner>
              { children }
             </Inner>
