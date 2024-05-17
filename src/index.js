15a80ea3cf8b91e20a9bc72d204953947b35838e
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-05-17 13:43:45 +0530

    Add - global-styles.js, adjust

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { GlobalStyles } from './global-styles'
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <> 
+
+    <GlobalStyles/> 
+    <App/>
+
+    </>  
+);
 
 
